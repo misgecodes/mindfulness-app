41b0149df9c5dec72810481eac34777be98cec93
Misgana Getachew <misganagetachew111@gmail.com>
2026-01-24 20:39:20 +0200

    basic rest api setup

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Category struct {
+	Id            string
+	Name          string
+	Description   string
+	Subcategories []string
+}
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// var categories = []Category{
+// 	{id: "1", name: "sprituality", description: "spritual contents", subcategories: []string{"meditation", "mindfulness"}},
+// }
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
 
 func main() {
-	fmt.Println("hey go how have you been doing")
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.Run("localhost:8080")
 }
